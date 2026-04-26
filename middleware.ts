1f61d43ba5e2fdd510c8bb16fa55f44c5b31a0a3
mohit-git1 <mohitsharma2826@gmail.com>
2026-04-26 11:02:03 +0530

    restore proper middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
-import { getToken } from "next-auth/jwt"
+import { auth } from "@/auth"
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.AUTH_SECRET
-  })
+  const session = await auth()
 
-  const isAuthenticated = !!token
+  const isAuthenticated = !!session
   const { pathname } = req.nextUrl
 
   if (pathname.startsWith("/api/auth")) {
