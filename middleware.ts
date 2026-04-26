d7a5f10f82df9268ac749ba95c92299964923245
mohit-git1 <mohitsharma2826@gmail.com>
2026-04-26 10:46:12 +0530

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -15,7 +15,9 @@ export async function middleware(req: NextRequest) {
     return NextResponse.next()
   }
 
-  const isPublicPath = pathname.startsWith('/signin') || pathname.startsWith('/signup')
+  const isPublicPath =
+    pathname.startsWith('/signin') ||
+    pathname.startsWith('/signup')
 
   if (!isAuthenticated && !isPublicPath) {
     return NextResponse.redirect(new URL('/signin', req.url))
@@ -29,5 +31,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)']
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 }
